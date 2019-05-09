f86b42b7f0a3318a8a3ebf0d047df85102c6830b
alfu32 <ioan.alferaru@gmail.com>
2019-05-09 11:55:03 +0200

    1 file modified

--- tab-layout.tsx
+++ tab-layout.tsx
@@ -1,45 +1,50 @@
 import React, { Component } from 'react';
 import { ComponentWrapper,MultislotTransclusionComponent } from './lib/base.components';
-import { kebapCase,classifyItems } from './lib/utils';
+import { guid,kebapCase,classifyItems,TaggedChildrenClassifier } from './lib/utils';
 
 export class TabTitle extends ComponentWrapper{}
-export class Tab extends Component{
+export class Tab extends Component implements TaggedChildrenClassifier{
   static classes = [TabTitle]
+  state={
+    id:guid(3,5)
+  }
+  classify(){
+    return classifyItems(this.props.children,[TabTitle])
+  }
   render(){
-    const classification = classifyItems(this.props.children, Tab.classes)
-    return <div>
+    const classification = this.classify();
+    return <div id={this.state.id}>
       <div className="tab-title">{classification['TabTitle']}</div>
-      <div className="tab-content">{classification['TabContent']}{classification['default']}</div>
+      <div className="tab-content">{classification['default']}</div>
     </div>
   }
 }
 
-export class Tabs extends Component{
+export class Tabs extends Component implements TaggedChildrenClassifier{
   
-    /** order counts */
-    static classes = [Tab]
   state={
-    sidebar:true,
-    color:true
+    currentId:guid(3,3)
   }
-  classifier(ch) {
-    return classifyItems(ch, Tabs.classes)
+  classify() {
+    const tabs = classifyItems(this.props.children, [Tab] )[ Tab.toString() ]
+    tabs( tab => {
+
+    })
   }
-  switchTab(){
-    this.setState({...this.state,sidebar:!this.state.sidebar});
+  switchTab(evt){
+    //this.setState({...this.state,currenttab:e});
     console.log('toggleSidebar',this.state);
   }
+  getCurrentTab(){
+    return Array.prototype.slice.call(this.props.children)
+      .filter( tab => tab.id===this.state.currentId )
+  }
   render(){
-    const classification = classifyItems(this.props.children,Tabs.classes);
+    const classification = this.classify();
 
-    const tabTitles=classification["Tab"].reduce( a,t => {
-      const cls=classifyItems(this.props.children,this.constructor.classes)
-      return a;
-    },{title:[],content:[]})
-    this.props.children.reduce
     return <div className="tabs-layout" sidebar-collapsed={this.state.sidebar.toString()}>
       <div className="tabs-titles"><div className='layout-button' onClick={this.toggleSidebar.bind(this)}></div>{classification['AppTitle']}</div>
-      <div className="tabs-content">{classification['AppSidebar']}</div>
+      <div className="tabs-content">{this.getCurrentTab()}</div>
     </div>
   }
 }
