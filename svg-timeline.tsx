dedfd7fa8e8e36e2d0329451995caa8c46ec5af6
alfu32 <ioan.alferaru@gmail.com>
2019-05-17 00:34:47 +0200

    1 file modified

--- svg-timeline.tsx
+++ svg-timeline.tsx
@@ -67,15 +67,15 @@ export class SvgTimeline extends Component{
     });
     const polyline = bufferImage.map( it => {
       return [
+        [-2,60],
         [-2,0],
-        [-2,10],
-        [2,10],
-        [2,0]
+        [2,0],
+        [2,60]
       ].map( p => `${it.time + p[0]},${p[1]}` ).join(` `)
     }).join(` `);
     return <svg width="300" height="60" style={{width: "300px", height: "60px", overflow: "visible", display: 'block', marginLeft:'30px' }} viewBox="0 0 300 60">
       <line x1="0" y1="30" x2={this.props['timeframe-length']/20} y2="30" style={{stroke:'rgb(255,0,0)',strokeWidth:2}}></line>
-      <polyline points={polyline} style={{fill: 'none', stroke: 'black', strokeWidth:3}} />
+      <polyline points={polyline} style={{fill: 'none', stroke: 'black', strokeWidth:1}} />
       { bufferImage.map( it => <Marble svg-color={it.color} pos-x={it.time}>{it.value}</Marble>) }
     </svg>
   }
