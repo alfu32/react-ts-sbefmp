70b8a30da1740fc3d83166ea65727015477bafc6
alfu32 <ioan.alferaru@gmail.com>
2019-05-16 17:49:19 +0200

    1 file modified, 1 added

--- app.component.old.tsx
+++ app.component.old.tsx
@@ -139,6 +139,76 @@ export class App extends Component {
                     renderer-factory={this.canvasRendererFactory}
                     canvas-model={this.canvasModel}><EventPipeDirective></EventPipeDirective></RXCanvas>
               </Tab>
+              <Tab>
+                <TabTitle>app layout 1</TabTitle>
+                  <h1>It Works</h1>
+                  <p>message</p>
+                  <b>  canvas </b>
+                  <RXCanvas
+                    on-inputEvent={this.onCanvasInputEvent}
+                    renderer-factory={this.canvasRendererFactory}
+                    canvas-model={this.canvasModel}><EventPipeDirective></EventPipeDirective></RXCanvas>
+              </Tab>
+              <Tab>
+                <TabTitle>app layout 2</TabTitle>
+                <div>
+                  <b> another app layout inside another one's content</b>
+                  <div style={{ position:"relative", minHeight:'440px',margin:'20px' }}>
+                    <AppLayout>
+                      <AppTitle>Layout 2</AppTitle>
+                      <AppSidebar>sb
+                      </AppSidebar>
+                      <AppToolbar>Toolbar</AppToolbar>
+                      <AppContent>
+                          <p>message 2</p>
+                          
+                          <b> multiple nested layouts </b>
+                          <div style={{ position:"relative", minHeight:'840px',margin:'20px' }}>
+                            <AppLayout>
+                              <AppTitle>subLayout 2.4</AppTitle>
+                              <AppSidebar>Sidebar</AppSidebar>
+                              <AppToolbar>Toolbar</AppToolbar>
+                              <AppContent>
+                                  <p>message 2.4</p>
+                                  { range(100).map( (v,i) => <pre>Line {i}</pre> )}
+                              </AppContent>
+                            </AppLayout>
+                          </div>
+                      </AppContent>
+                    </AppLayout>
+                  </div>
+                </div>
+              </Tab>
+              <Tab>
+                <TabTitle>app layout 3</TabTitle>
+                <div>
+                  <b> a third app layout inside main app layout </b>
+                  <div style={{ position:"relative", minHeight:'440px',margin:'20px' }}>
+                    <AppLayout>
+                      <AppTitle>Layout 2</AppTitle>
+                      <AppSidebar>Sidebar</AppSidebar>
+                      <AppToolbar>Toolbar</AppToolbar>
+                      <AppContent>
+                          <p>message 3</p>
+                      </AppContent>
+                      <AppStatusbar>
+                        <em>status 3</em>
+                      </AppStatusbar>
+                    </AppLayout>
+                  </div>
+                </div>
+              </Tab>
+              <Tab>
+                <TabTitle>more tabs </TabTitle>
+                <Tabs>
+                  { range(7).map( (v,i) => <Tab>
+                    <TabTitle>sub tab 4.{i}</TabTitle>
+                    <div>
+                      content 4.{i}
+                    </div>
+                  </Tab>)}
+                </Tabs>
+              </Tab>
             </Tabs>
         </AppContent>
       </AppLayout>
